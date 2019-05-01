24b8fc72dba7f0301fdd39bc499502ada6c28e56
JACK FLYNN <jack.flynn19@clcmbp7.local>
2019-05-01 08:54:07 -0500

    Good Job!

--- Pick Up Game/ViewController3.swift
+++ Pick Up Game/ViewController3.swift
@@ -8,12 +8,43 @@
 
 import UIKit
 
-class ViewController3: UIViewController {
-
+class ViewController3: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+   
+    @IBOutlet weak var whenPlaying: UIDatePicker!
+    @IBOutlet weak var wheretext: UITextField!
+    @IBOutlet weak var whatSportPick: UIPickerView!
+    var sports = ["Basketball","Football","Baseball","Hockey","Volleyball","Spikeball"]
+    var whatSport = ""
+    var wherePlay = ""
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sports[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+       return sports.count
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        whatSport = sports[row]
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+   
+        
+        
 
-
+    }
+    @IBAction func startGameButton(_ sender: Any) {
+   wheretext.text = wherePlay
+    
     }
     
 
