8efe0db604a41170b40271ceab36841ea9ff8968
JACK FLYNN <jack.flynn19@clcmbp7.local>
2019-05-02 08:55:50 -0500

    Gucci

--- Pick Up Game/ViewController3.swift
+++ Pick Up Game/ViewController3.swift
@@ -16,7 +16,8 @@ class ViewController3: UIViewController, UIPickerViewDelegate, UIPickerViewDataS
     var sports = ["Basketball","Football","Baseball","Hockey","Volleyball","Spikeball"]
     var whatSport = ""
     var wherePlay = ""
-    
+    var when = ""
+   
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -37,13 +38,45 @@ class ViewController3: UIViewController, UIPickerViewDelegate, UIPickerViewDataS
     
     override func viewDidLoad() {
         super.viewDidLoad()
-   
+        
+        let datePicker: UIDatePicker = UIDatePicker()
+        
+        // Posiiton date picket within a view
+        datePicker.frame = CGRect(x: 10, y: 50, width: self.view.frame.width, height: 200)
+        
+        // Set some of UIDatePicker properties
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.backgroundColor = UIColor.white
+        
+        // Add an event to call onDidChangeDate function when value is changed.
+        datePicker.addTarget(self, action: #selector(ViewController3.datePickerValueChanged(_:)), for: .valueChanged)
+        
+        // Add DataPicker to the view
+        self.view.addSubview(datePicker)
+        
+    }
+    
+    
+    @objc func datePickerValueChanged(_ sender: UIDatePicker){
+        
+        // Create date formatter
+        let dateFormatter: DateFormatter = DateFormatter()
+        
+        // Set date format
+        dateFormatter.dateFormat = "MM/dd/yyyy hh:mm a"
+        
+        // Apply date format
+        let selectedDate: String = dateFormatter.string(from: sender.date)
+        
+        print("Selected value \(selectedDate)")
         
         
 
     }
     @IBAction func startGameButton(_ sender: Any) {
    wheretext.text = wherePlay
+   
+   
     
     }
     
