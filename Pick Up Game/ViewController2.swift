b022620e3b00ed159b77f7935fa0ae24c1fb9342
COLE WHITE <cole.white19@clcmbp25.local>
2019-05-02 08:55:19 -0500

    Worked on second view controller

--- Pick Up Game/ViewController2.swift
+++ Pick Up Game/ViewController2.swift
@@ -1,23 +1,21 @@
-//
-//  ViewController2.swift
-//  Pick Up Game
-//
-//  Created by JACK FLYNN on 4/26/19.
-//  Copyright © 2019 clc.Flynn. All rights reserved.
-//
-
 import UIKit
+var Games = ["Fake Game"]
+
 
 class ViewController2: UIViewController {
     var Nickname: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var retrieve = UserDefaults.standard
 
     }
-    
-    @IBAction func newGame(_ sender: UIBarButtonItem) {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Games.count
     }
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "White")!
+        cell.textLabel?.text = Games[indexPath.row]
+        return cell
+    }
 
 }
