33c634213c1330e3002fe4a13d2f06f36d7b0e67
COLE WHITE <cole.white19@clcmbp25.local>
2019-05-08 08:46:27 -0500

    Hell yeah

--- Pick Up Game/ViewController.swift
+++ Pick Up Game/ViewController.swift
@@ -7,19 +7,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        func shouldPerformSegueWithIdentifier(identifier: String, sender: UIButton)->Bool{
-            if identifier == "firstsegue" { // you define it in the storyboard (click on the segue, then Attributes' inspector > Identifier
-                
-                if Name.text?.isEmpty == true {
-                    Warning.text="Please Enter a Name"
-                    return false
-                }
-
-            }
-            
-            // by default, transition
-            return true
-        }
+       
 
     }
 
@@ -28,6 +16,46 @@ class ViewController: UIViewController {
         nvc.Nickname = Name
 
     }
+    @IBAction func shouldPerformSegueWithIdentifier(_ sender: UIButton) {
+        if !Name.text!.isEmpty
+        {
+            //Warning.text = "Enter a Name"
+            performSegue(withIdentifier: "firstsegue", sender: nil)
+        }
+        else
+        {
+            //performSegue(withIdentifier: "firstsegue", sender: nil)
+            Warning.text = "Please Write a Name"
+        }
+    }
+    /*func shouldPerformSegueWithIdentifier(identifier: String, sender: UIButton)->Bool{
+        if !Name.text!.isEmpty
+        {
+            Warning.text = "You didn't actually write anything."
+            //performSegue(withIdentifier: "firstsegue", sender: sender)
+        }
+        else
+        {
+            performSegue(withIdentifier: "firstsegue", sender: sender)
+           // Warning.text = "You didn't actually write anything."
+        }
+ 
+        
+       /* if identifier == "firstsegue" { // you define it in the storyboard (click on the segue, then Attributes' inspector > Identifier
+            
+            if Name.text?.isEmpty == true {
+                Warning.text="Please Enter a Name"
+                return false
+            }
+            else{
+                return true
+            }
+            
+        }*/
+        
+        // by default, transition
+        return true
+    }*/
     
     
     }
