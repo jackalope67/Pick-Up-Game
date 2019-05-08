3f94859024920b59bffec7e0997371d764f4c4c5
JACK FLYNN <jack.flynn19@clcmbp7.local>
2019-05-08 08:55:40 -0500

    hi

--- Pick Up Game/ViewController2.swift
+++ Pick Up Game/ViewController2.swift
@@ -5,19 +5,19 @@ class ViewController2: UIViewController {
     var Nickname: UITextField!
     var Games = ["Fake Game"]
     var yes: String!
-
     var no: String!
-    override func viewDidLoad() {
+    
+override func viewDidLoad() {
         super.viewDidLoad()
         var retrieve = UserDefaults.standard
-print(no)
+
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return Games.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "White")!
-        cell.textLabel?.text = Games[indexPath.row]
+        cell.textLabel?.text = yes
         return cell
     }
   
